d48dfc7a74248b67499b8f6225dd6b0a449e3718
krkimg <coca970105@naver.com>
2022-07-14 16:13:54 +0900

    commit

--- Programmers_Level1/로또의최고순위와최저순위.py
+++ Programmers_Level1/로또의최고순위와최저순위.py
@@ -5,11 +5,18 @@ def solution(lottos, win_nums):
     for i in range(len(lottos)):
         if lottos[i] in win_nums:
             count+=1
-          
+
+
     for i in range(len(lottos)):
         if lottos[i]==0:
             count2+=1
+    if count+count2 ==0:
+        answer.append(6-count-count2)
+    else:
+        answer.append(6-count-count2+1)
+    if count==0:
+        answer.append(6-count)
+    else:
+        answer.append(6-count+1)
 
-    answer.append(6-count-count2,6-count)
-              
     return answer
